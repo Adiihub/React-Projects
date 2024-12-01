a5e6f9d53fb52c6144d1bdbe20a3e586075b0241
Aditi Kumari <aditi22202@gmail.com>
2024-12-01 11:33:37 +0530

    Context-API

---
+++ 05MiniContext/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './Context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
